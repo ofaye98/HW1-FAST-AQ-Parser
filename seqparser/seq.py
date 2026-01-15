ddb07e54324aaf20042a9184bf4e5a206f9ce19b
Faye Orcales <75501045+ofaye98@users.noreply.github.com>
2026-01-14 16:51:06 -0800

    wrote functions

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,16 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    tscb_seq = seq.replace("T", "U")
+    return tscb_seq
+    #pass
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    tscb_seq = transcribe(seq)
+    rv_tscb_seq = tscb_seq[::-1]
+    return rv_tscb_seq
+    #pass
