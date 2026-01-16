70b829b01bb2d5abe93bd0cb435dc173e88532e4
Faye Orcales <75501045+ofaye98@users.noreply.github.com>
2026-01-15 22:42:19 -0800

    added unit tests

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -4,6 +4,7 @@ from seqparser import (
         transcribe,
         reverse_transcribe)
 
+import pytest
 
 def test_freebie_transcribe_1():
     """
@@ -25,11 +26,22 @@ def test_transcribe():
     """
     Write your unit test for the transcribe function here.
     """
-    pass
+    assert transcribe("ACTG") == "UGAC"
+    assert transcribe("actg") == "UGAC"   # lowercase input should work
+    assert transcribe("AcTg") == "UGAC"   # lower/uppercase input should work
 
+    # check if user inputted an invalid nucleotide (ex. X)
+    with pytest.raises(ValueError, match=r"Invalid nucleotide:\s*X"):
+        transcribe("ACTX")
 
 def test_reverse_transcribe():
     """
     Write your unit test for the reverse transcribe function here.
     """
-    pass
+    assert reverse_transcribe("ACTG") == "CAGU"
+    assert reverse_transcribe("actg") == "CAGU"   # lowercase input should work
+    assert reverse_transcribe("AcTg") == "CAGU"   # lower/uppercase input should work
+
+    # check if user inputted an invalid nucleotide (ex. X)
+    with pytest.raises(ValueError, match=r"Invalid nucleotide:\s*X"):
+        reverse_transcribe("ACTX")
